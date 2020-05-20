1fb4a1b7254e7fe36226fdb730afb9906527f1bd
DariuszLegizynski <d.legizynski@gmail.com>
2020-05-20 09:56:15 +0200

    use setState to render the latitude of my geolocatin and also added a conditional statement that is handling any error messages

--- src/components/GeoLocation.js
+++ src/components/GeoLocation.js
@@ -1,17 +1,29 @@
 import React from "react";
 
 class GeoLocation extends React.Component{
-    render(){
+    constructor(props){
+        super(props);
+
+        this.state={lat: null, errorMessage: "" };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            position => {
+                this.setState({lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({errorMessage: err.message});
+            }
         );
+    }
     
-        return(
-            <div>
-                Latitude:
-            </div>
-        );
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Latitude: Error, {this.state.errorMessage}</div>
+        } else if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        } else{
+            return<div>Loading, please wait.</div>
+        }
     }
 }
 
